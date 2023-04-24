7c1867fba6285ab27aa79076638994de053f0eb5
Samishan11 <samishanthapa0@gmail.com>
2023-04-24 20:16:26 +0545

    setup complete

--- resume/urls.py
+++ resume/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/', views.upload_resume , name="resume_upload"),
+    path('', views.upload_resume),
         path('resume_summary/<int:resume_id>', views.summarize_text, name='resume_summary'),
 
 ]
