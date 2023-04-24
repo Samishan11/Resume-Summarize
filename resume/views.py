e94a55978c4e8ddc1ed3c1444e466ef8f0f40ceb
Samishan11 <samishanthapa0@gmail.com>
2023-04-24 21:15:13 +0545

    chnages

--- resume/views.py
+++ resume/views.py
@@ -8,19 +8,30 @@ import spacy
 def upload_resume(request):
     if request.method == 'POST':
         form = ResumeUploadForm(request.POST, request.FILES)
+        print("aahah")
         if form.is_valid():
             # Save the uploaded resume to the user profile
             user_profile = UserProfile.objects.get(user=request.user)
             user_profile.resume = form.cleaned_data['resume']
             user_profile.save()
-            # Summarize the resume using spaCy and return the summary
-            text = textract.process(user_profile.resume)
+            print('user_profile',user_profile)
+            
+            # Summarize the resume using spaCy
+            file_path = user_profile.resume.path
+            print('file_path',file_path)
+            text = textract.process(file_path)
+            print('text',text)
             nlp = spacy.load('en_core_web_sm')
             doc = nlp(text)
+            print('doc',doc)
             summary = summarize_text(doc)
-            return HttpResponse(summary)
+            print('summary',summary)
+            
+            # Render a new template with the summary
+            return render(request, 'resume_upload.html', {'summary': summary})
     else:
         form = ResumeUploadForm()
+        print("err")
     return render(request, 'resume_upload.html', {'form': form})
 
 def summarize_text(doc):
@@ -52,4 +63,5 @@ def summarize_text(doc):
             skills.append(token.text)
     # Generate a summary of the most important information
     summary = 'Name: {}\n\nContact Information: {}\n\nWork Experience: {}\n\nEducation: {}\n\nSkills: {}'.format(name, ', '.join(contact_info), ', '.join(work_experience), ', '.join(education), ', '.join(skills))
+    print('summary1',summary)
     return summary
--- resume_parser/settings.py
+++ resume_parser/settings.py
@@ -80,8 +80,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
