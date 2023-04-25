422ad8966380b131febefad435dc161a37810082
Samishan11 <samishanthapa0@gmail.com>
2023-04-25 20:14:39 +0545

    changes

--- resume/models.py
+++ resume/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    resume = models.FileField(upload_to='resumes/')
+    resume = models.FileField(upload_to="resumes/")
--- resume/views.py
+++ resume/views.py
@@ -1,3 +1,4 @@
+import os
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import UserProfile
@@ -5,63 +6,74 @@ from .forms import ResumeUploadForm
 import textract
 import spacy
 
+
 def upload_resume(request):
-    if request.method == 'POST':
-        form = ResumeUploadForm(request.POST, request.FILES)
-        print("aahah")
-        if form.is_valid():
-            # Save the uploaded resume to the user profile
-            user_profile = UserProfile.objects.get(user=request.user)
-            user_profile.resume = form.cleaned_data['resume']
-            user_profile.save()
-            print('user_profile',user_profile)
-            
-            # Summarize the resume using spaCy
-            file_path = user_profile.resume.path
-            print('file_path',file_path)
-            text = textract.process(file_path)
-            print('text',text)
-            nlp = spacy.load('en_core_web_sm')
-            doc = nlp(text)
-            print('doc',doc)
-            summary = summarize_text(doc)
-            print('summary',summary)
-            
-            # Render a new template with the summary
-            return render(request, 'resume_upload.html', {'summary': summary})
-    else:
-        form = ResumeUploadForm()
-        print("err")
-    return render(request, 'resume_upload.html', {'form': form})
+    try:
+        if request.method == "POST":
+            form = ResumeUploadForm(request.POST, request.FILES)
+            if form.is_valid():
+                # Save the uploaded resume to the user profile
+                user_profile = UserProfile.objects.get(user=request.user)
+                user_profile.resume = form.cleaned_data["resume"]
+                user_profile.save()
+
+                # Update file path format to forward slashes
+                file_path = user_profile.resume.file.name.replace("\\", "/")
+
+                # Summarize the resume using spaCy
+                print(file_path)
+                # text = textract.process(file_path)
+                # nlp = spacy.load("en_core_web_sm")
+                # doc = nlp(text)
+                # summary = summarize_text(doc)
+
+                # # Render a new template with the summary
+                return render(
+                    request,
+                    "resume_upload.html",
+                    # {"summary": summary},
+                )
+        else:
+            form = ResumeUploadForm()
+            return render(request, "resume_upload.html", {"form": form})
+    except Exception as e:
+        print("error", e)
+        return HttpResponse("An error occurred while processing your request.")
+
 
 def summarize_text(doc):
     # Extract the candidate's name
     name = None
     for ent in doc.ents:
-        if ent.label_ == 'PERSON':
+        if ent.label_ == "PERSON":
             name = ent.text
             break
     # Extract the candidate's contact information
     contact_info = []
     for ent in doc.ents:
-        if ent.label_ == 'PHONE_NUMBER' or ent.label_ == 'EMAIL':
+        if ent.label_ == "PHONE_NUMBER" or ent.label_ == "EMAIL":
             contact_info.append(ent.text)
     # Extract the candidate's work experience
     work_experience = []
     for ent in doc.ents:
-        if ent.label_ == 'ORG':
+        if ent.label_ == "ORG":
             work_experience.append(ent.text)
     # Extract the candidate's education
     education = []
     for ent in doc.ents:
-        if ent.label_ == 'EDUCATION':
+        if ent.label_ == "EDUCATION":
             education.append(ent.text)
     # Extract the candidate's skills
     skills = []
     for token in doc:
-        if token.pos_ == 'NOUN':
+        if token.pos_ == "NOUN":
             skills.append(token.text)
     # Generate a summary of the most important information
-    summary = 'Name: {}\n\nContact Information: {}\n\nWork Experience: {}\n\nEducation: {}\n\nSkills: {}'.format(name, ', '.join(contact_info), ', '.join(work_experience), ', '.join(education), ', '.join(skills))
-    print('summary1',summary)
+    summary = "Name: {}\n\nContact Information: {}\n\nWork Experience: {}\n\nEducation: {}\n\nSkills: {}".format(
+        name,
+        ", ".join(contact_info),
+        ", ".join(work_experience),
+        ", ".join(education),
+        ", ".join(skills),
+    )
     return summary
