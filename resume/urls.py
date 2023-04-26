f04dfab59c9e3b505c39ae6859896555e835bb8e
Samishan11 <samishanthapa0@gmail.com>
2023-04-26 07:35:33 +0545

    changes

--- resume/urls.py
+++ resume/urls.py
@@ -2,7 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.upload_resume),
-        path('resume_summary/<int:resume_id>', views.summarize_text, name='resume_summary'),
-
+    path("", views.upload_resume),
 ]
--- resume/views.py
+++ resume/views.py
@@ -3,8 +3,50 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import UserProfile
 from .forms import ResumeUploadForm
-import textract
-import spacy
+from PyPDF2 import PdfReader
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.summarizers.lex_rank import LexRankSummarizer
+import nltk
+
+nltk.download("punkt")
+
+
+# def extract_experiences_skills(text):
+#     """
+#     Extracts 'Experiences' and 'Skills' sections from the text.
+#     Assumes that 'Experiences' section comes before 'Skills' section in the text.
+#     """
+#     experiences = ""
+#     skills = ""
+#     keyword_experiences = "Experiences"
+#     keyword_skills = "Skills"
+#     # Split the text by newline to get each line
+#     lines = text.split("\n")
+#     for line in lines:
+#         if keyword_experiences in line:
+#             # Extract 'Experiences' section
+#             experiences = line.replace(keyword_experiences, "").strip()
+#         elif keyword_skills in line:
+#             # Extract 'Skills' section
+#             skills = line.replace(keyword_skills, "").strip()
+
+#     return experiences, skills
+
+
+def extract_experiences_and_skills(text):
+    # print(text)
+    experiences = []
+    skills = []
+    lines = text.split("\n")
+    for line in lines:
+        print(line)
+        if line.lower().startswith("experiences"):
+            experiences = [exp.strip() for exp in line.split(":")[1].split(",")]
+        elif line.lower().startswith("skills"):
+            print(line.lower().startswith("skills"))
+            skills = [skill.strip() for skill in line.split(":")[1].split(",")]
+    return experiences, skills
 
 
 def upload_resume(request):
@@ -18,20 +60,48 @@ def upload_resume(request):
                 user_profile.save()
 
                 # Update file path format to forward slashes
-                file_path = user_profile.resume.file.name.replace("\\", "/")
+                file_path = user_profile.resume.file.name
 
-                # Summarize the resume using spaCy
-                print(file_path)
-                # text = textract.process(file_path)
-                # nlp = spacy.load("en_core_web_sm")
-                # doc = nlp(text)
-                # summary = summarize_text(doc)
+                # Extract text from PDF file using PdfReader
+                with open(file_path, "rb") as file:
+                    pdf_reader = PdfReader(file)
+                    text = ""
+                    for page_num in range(len(pdf_reader.pages)):
+                        page = pdf_reader.pages[page_num]
+                        text += page.extract_text()
 
-                # # Render a new template with the summary
+                    # Parse the text from the PDF file
+                    parser = PlaintextParser.from_string(text, Tokenizer("english"))
+                    summarizer = LexRankSummarizer()
+                    # Get the summary with a specified number of sentences (e.g., 3)
+                    summary = summarizer(parser.document, sentences_count=3)
+
+                    # Extract the summarized text from the summary
+                    summarized_text = [str(sentence) for sentence in summary]
+
+                    # Convert the summarized text back to a single string
+                    summarized_text = " ".join(summarized_text)
+
+                # Extract 'Experiences' and 'Skills' sections from the summarized text
+                # experiences, skills = extract_experiences_skills(summarized_text)
+
+                experiences, skills = extract_experiences_and_skills(summarized_text)
+
+                # Print the extracted 'Experiences' and 'Skills' sections
+                # print("Experiences:", experiences)
+                # print("Skills:", skills)
+                print("Experiences:")
+                for experience in experiences:
+                    print("- " + experience)
+
+                print("Skills:")
+                for skill in skills:
+                    print("- " + skill)
+
                 return render(
                     request,
                     "resume_upload.html",
-                    # {"summary": summary},
+                    {"summarized_text": summarized_text, "skills": skills},
                 )
         else:
             form = ResumeUploadForm()
@@ -39,41 +109,3 @@ def upload_resume(request):
     except Exception as e:
         print("error", e)
         return HttpResponse("An error occurred while processing your request.")
-
-
-def summarize_text(doc):
-    # Extract the candidate's name
-    name = None
-    for ent in doc.ents:
-        if ent.label_ == "PERSON":
-            name = ent.text
-            break
-    # Extract the candidate's contact information
-    contact_info = []
-    for ent in doc.ents:
-        if ent.label_ == "PHONE_NUMBER" or ent.label_ == "EMAIL":
-            contact_info.append(ent.text)
-    # Extract the candidate's work experience
-    work_experience = []
-    for ent in doc.ents:
-        if ent.label_ == "ORG":
-            work_experience.append(ent.text)
-    # Extract the candidate's education
-    education = []
-    for ent in doc.ents:
-        if ent.label_ == "EDUCATION":
-            education.append(ent.text)
-    # Extract the candidate's skills
-    skills = []
-    for token in doc:
-        if token.pos_ == "NOUN":
-            skills.append(token.text)
-    # Generate a summary of the most important information
-    summary = "Name: {}\n\nContact Information: {}\n\nWork Experience: {}\n\nEducation: {}\n\nSkills: {}".format(
-        name,
-        ", ".join(contact_info),
-        ", ".join(work_experience),
-        ", ".join(education),
-        ", ".join(skills),
-    )
-    return summary
